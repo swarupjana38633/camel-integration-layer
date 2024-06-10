5aa8bff6501d7177cc6ad8d7402af206f65d96a6
swarupjana38633 <swarup.jana@thoughtworks.com>
2024-06-10 18:46:00 +0530

    changes done for Jmeter load testing

--- src/main/java/com/hli/camel/boot/controller/CamelController.java
+++ src/main/java/com/hli/camel/boot/controller/CamelController.java
@@ -1,2 +1,27 @@
-package com.hli.camel.boot.controller;public class CamelController {
+package com.hli.camel.boot.controller;
+
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CamelController {
+
+    @Autowired
+    ProducerTemplate producerTemplate;
+
+    @Autowired
+    ConsumerTemplate consumerTemplate;
+
+    @GetMapping(value = "/jsonToMQ", produces = "text/plain")
+    public String jsonToMQ() {
+        return producerTemplate.requestBody("direct:JSON_TO_FIXED_LENGTH", "", String.class);
+    }
+
+    @GetMapping(value = "/mqToJson", produces = "text/plain")
+    public String mqToJson() {
+        return producerTemplate.requestBody("direct:FIXED_LENGTH_TO_JSON", "", String.class);
+    }
 }
